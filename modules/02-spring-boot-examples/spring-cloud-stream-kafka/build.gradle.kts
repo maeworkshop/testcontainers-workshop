9e7b3566283edfd53a997fac250dfb613ffa5050
Emre Sen <maemresen@yazilim.vip>
2024-01-21 18:20:27 +0200

    module created

---
+++ modules/02-spring-boot-examples/spring-cloud-stream-kafka/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+repositories {
+    mavenCentral()
+}
+
+extensions.configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_21 // Replace with your desired version
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+dependencies {
+
+    // Spring Boot Deps
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Testcontainers deps
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+
+    // Other (Optional)
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    testImplementation("io.rest-assured:rest-assured")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,4 @@ rootProject.name = "testcontainers-workshop"
 
 include(":modules:01-basic-examples")
 include(":modules:02-spring-boot-examples:basics")
+include(":modules:02-spring-boot-examples:spring-cloud-stream-kafka")
